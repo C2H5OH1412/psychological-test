f02fffc2396320057cea1b23feea7ac5b3744ce0
C2H5OH1412 <stephanie920319@gmail.com>
2025-06-12 03:50:10 +0800

    fix: add Tailwind content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 // tailwind.config.js
 module.exports = {
+    content: [
+      "./src/**/*.{js,ts,jsx,tsx}", // 加上這行！讓 Tailwind 找到你 src 內所有檔案
+    ],
     theme: {
       extend: {
         fontFamily: {
@@ -7,5 +10,6 @@ module.exports = {
         },
       },
     },
+    plugins: [],
   };
   
