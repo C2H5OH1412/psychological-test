f99aaae76a913c2a5554c65c9778938d41b2eea1
C2H5OH1412 <stephanie920319@gmail.com>
2025-06-12 03:27:21 +0800

    0612-3

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,5 @@
-/* 
-
+import StartPage from '@/component/StartPage'; // 根據你的實際檔案路徑
 
 export default function Home() {
-  return (
-    <>
-      <div>HOME</div>
-      <div>HOME</div>
-    </>
-  );
+  return <StartPage />;
 }
- */
